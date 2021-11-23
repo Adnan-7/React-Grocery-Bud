e0319df52f3ebef2ff92cafdd61a304a630baa14
Adnan Khan <adnan7.zain@gmail.com>
2021-11-23 22:01:10 +0500

    design a basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Alert from './alert';
+import List from './list';
 
 function App() {
-  return <div>grocery bud setup</div>;
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [IsEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', type: '' });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  };
+
+  return (
+    <section className='section-center'>
+      <form onSubmit={handleSubmit} className='grocery-form'>
+        {alert.show && <Alert />}
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g. eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button className='submit-btn'>
+            {IsEditing ? 'Edit' : 'Submit'}
+          </button>
+        </div>
+      </form>
+      <div className='grocery-container'>
+        <List />
+        <button className='clear-btn'>clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
